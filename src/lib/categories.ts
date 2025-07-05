d888755f9ba0fe035ee86e13e7f096f1f19c2ba1
Nihar Mondal <developernihar@gmail.com>
2025-07-05 17:50:34 +0600

    api: category data is forced to cache

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -4,7 +4,7 @@ export async function getCategories(): Promise<IResponse<ICategories[]>> {
 	try {
 		const res = await fetch(
 			"http://157.230.240.97:9999/api/v1/categories",
-			{ next: { revalidate: 50000 } }
+			{ cache: "force-cache" }
 		);
 		const categories = await res.json();
 		return categories;
